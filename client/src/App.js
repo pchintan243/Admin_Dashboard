f32adaa8ea413034fa85ed9af06d6eefc2d7e8b7
chintan <patelchintan843@gmail.com>
2023-05-14 17:11:43 +0530

    sidebar created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import "./app.css";
 
 function App() {
   return (
     <>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+    <div className="others">Other Pages</div>
+      </div>
     </>
   );
 }
