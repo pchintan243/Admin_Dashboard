13ad7b9e038b6e9998bc8253ed3b80d090f116f9
chintan <patelchintan843@gmail.com>
2023-05-14 16:19:43 +0530

    topbar complete

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { NotificationsNone, Language, Settings } from '@material-ui/icons';
+import "./topbar.css"
 
 function Topbar() {
-  return (
-    <div>Topbar</div>
-  )
+    return (
+        <div className='topbar'>
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">AdminDashboard</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsNone />
+                        <span className="topIconBadge">
+                            2
+                        </span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Language />
+                        <span className="topIconBadge">
+                            2
+                        </span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Settings />
+                    </div>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9aFd3RDK6JfPNwLWN4FfVCEDJTHmwoPIpUWScW9NXuA&ec=48665701" alt="" className="topAvatar" />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Topbar
