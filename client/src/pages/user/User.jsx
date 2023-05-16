a093d2bdc3ee0f73bbe8cd30f3f87e683d3a5a51
chintan <patelchintan843@gmail.com>
2023-05-16 10:55:09 +0530

    user update page

--- client/src/pages/user/User.jsx
+++ client/src/pages/user/User.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import "./user.css"
-import { CalendarToday, LocationSearching, MailOutline, PermIdentity, PhoneAndroid } from '@material-ui/icons'
+import { CalendarToday, LocationSearching, MailOutline, PermIdentity, PhoneAndroid, Publish } from '@material-ui/icons'
+import { Link } from 'react-router-dom'
 
 const User = () => {
     return (
         <div className='user'>
             <div className="userTitleContainer">
                 <h1 className="userTitle">Edit User</h1>
-                <button className="userAddButton">Create</button>
+                <Link to="/newUser">
+                    <button className="userAddButton">Create</button>
+                </Link>
             </div>
             <div className="userContainer">
                 <div className="userShow">
@@ -53,8 +56,31 @@ const User = () => {
                                 <label htmlFor="">Username</label>
                                 <input type="text" placeholder="alley56" className="userUpdateInput" />
                             </div>
+                            <div className="userUpdateItem">
+                                <label htmlFor="">Full Name</label>
+                                <input type="text" placeholder="Deven alley" className="userUpdateInput" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label htmlFor="">Email</label>
+                                <input type="text" placeholder="Hello@gmail.co.in" className="userUpdateInput" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label htmlFor="">Phone</label>
+                                <input type="text" placeholder="+91 1234567890" className="userUpdateInput" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label htmlFor="">Address</label>
+                                <input type="text" placeholder="Miami | USA" className="userUpdateInput" />
+                            </div>
                         </div>
-                        <div className="userUpdateRIght"></div>
+                        <div className="userUpdateRight">
+                            <div className="userUpdateUpload">
+                                <img src="https://images.healthshots.com/healthshots/en/uploads/2020/12/08182549/positive-person.jpg" alt="" className="userUpdateImg" />
+                                <label htmlFor="file"><Publish className='userUpdateIcon' /></label>
+                                <input type="file" id="file" style={{ display: "none" }} />
+                            </div>
+                            <button className="userUpdateButton">Update</button>
+                        </div>
                     </form>
                 </div>
             </div>
