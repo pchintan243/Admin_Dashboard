42581bd96385d25de7286ff6e582d255a5c9ffbc
chintan <patelchintan843@gmail.com>
2023-05-16 13:07:45 +0530

    add newproduct form page

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 
 function App() {
   return (
@@ -27,7 +28,7 @@ function App() {
             <Route exact path="/newUser" element={<NewUser />} />
             <Route exact path="/products" element={<ProductList />} />
             <Route exact path="/product/:productId" element={<Product />} />
-            <Route exact path="/newproduct" element={<NewUser />} />
+            <Route exact path="/newproduct" element={<NewProduct />} />
           </Routes>
         </div>
       </Router>
